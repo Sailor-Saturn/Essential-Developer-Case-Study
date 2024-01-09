0793958fc660bdea9a69b119a0502c8b6550c27d
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-09 13:01:01 +0000

    Garantee we only receive one error

--- essential-feed-case-study/essential-feed-case-studyTests/RemoteFeedLoaderTests.swift
+++ essential-feed-case-study/essential-feed-case-studyTests/RemoteFeedLoaderTests.swift
@@ -36,11 +36,11 @@ final class RemoteFeedLoaderTests: XCTestCase {
         // Stubbing the client
         client.error = NSError(domain: "Test", code: 0)
         
-        var capturedError: RemoteFeedLoader.Error?
+        var capturedErrors = [RemoteFeedLoader.Error]()
         
-        sut.load { error in capturedError = error }
+        sut.load { capturedErrors.append($0) }
         
-        XCTAssertEqual(capturedError, .connectivity)
+        XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
     // MARK: Helpers
