72d76168de19fe492ebc66757fb019f4d2f5088f
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-11 16:11:22 +0000

    Simplify type declarations with a helper function

--- essential-feed-case-study/essential-feed-case-studyTests/Feed API/RemoteFeedLoaderTests.swift
+++ essential-feed-case-study/essential-feed-case-studyTests/Feed API/RemoteFeedLoaderTests.swift
@@ -32,7 +32,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSut()
         expect(sut,
-               toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity),
+               toCompleteWith: failure(.connectivity),
                when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
@@ -45,7 +45,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut, toCompleteWith: failure(.invalidData), when: {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             })
@@ -55,7 +55,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSut()
         
-        expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
@@ -125,6 +125,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         trackForMemoryLeaks(client)
         
         return (sut, client)
+    }
+    
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
     }
     
     private func trackForMemoryLeaks(
