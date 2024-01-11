fee9fa8f628c2504989dddd6cb73c0f51dde977d
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-11 12:08:07 +0000

    Move HTTPClient to it's own file.

---
+++ essential-feed-case-study/essential-feed-case-study/FeedAPI/HTTPClient.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(
+        from url: URL,
+        completion: @escaping (HTTPClientResult) -> Void
+    )
+}
