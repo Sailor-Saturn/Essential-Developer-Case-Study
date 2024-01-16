8cbbafa79272aff08cbfc5667db450c6baa72031
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-16 15:45:52 +0000

    Change timeout to 10 seconds since CI is failing on 6 seconds

--- EssentialDeveloper/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialDeveloper/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -40,7 +40,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10)
         return receivedResult
     }
     
