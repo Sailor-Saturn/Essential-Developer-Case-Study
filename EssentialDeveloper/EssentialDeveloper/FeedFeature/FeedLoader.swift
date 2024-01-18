d10d50d271168fb2b43a97fe38f81467ac394713
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-18 10:38:04 +0000

    Add files to folder that were being read as reference from the other deleted project

---
+++ EssentialDeveloper/EssentialDeveloper/FeedFeature/FeedLoader.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
+public protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
