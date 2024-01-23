1b80fc56e81c3b4692b055876421d89897d599ae
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-23 14:40:14 +0000

    Validate cache command does not delete less than seven days old cache

--- EssentialDeveloper/EssentialDeveloperTests/Feed Cache/LoadFeedFromCacheTests.swift
+++ EssentialDeveloper/EssentialDeveloperTests/Feed Cache/LoadFeedFromCacheTests.swift
@@ -77,7 +77,7 @@ final class LoadFeedFromCacheTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
-    func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+    func test_load_hasNoSideEffectsOnLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: 7).adding(seconds: 1)
--- EssentialDeveloper/EssentialDeveloperTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialDeveloper/EssentialDeveloperTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -29,6 +29,34 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_validate_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: 7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.validateCache()
+        
+        store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueImage(), uniqueImage()]
+        let localItems = models.map {
+            LocalFeedImage(id: $0.id, description:  $0.description, location: $0.location, url: $0.url)}
+        return (models, localItems)
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
+    }
+    
     private func anyNSError() -> NSError {
         return NSError(domain: "any eror", code: 0)
     }
@@ -43,5 +71,15 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
+    }
+}
+
+private extension Date {
+    func adding (days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding (seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
