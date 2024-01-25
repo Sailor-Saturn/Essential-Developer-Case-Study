132d0949b847995f4803133645b953adb353f3e1
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-25 16:05:46 +0000

    Add comment to document that the completion handler in any http client implementation can be invoked in any thread.

--- EssentialDeveloper/EssentialDeveloper/FeedAPI/HTTPClient.swift
+++ EssentialDeveloper/EssentialDeveloper/FeedAPI/HTTPClient.swift
@@ -6,8 +6,7 @@ public enum HTTPClientResult {
 }
 
 public protocol HTTPClient {
-    func get(
-        from url: URL,
-        completion: @escaping (HTTPClientResult) -> Void
-    )
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
