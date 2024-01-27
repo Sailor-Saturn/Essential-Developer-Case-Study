6c90a879cbbc511bc45f79ce396d6affc603ff07
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-27 15:28:20 +0000

    add clean up of artifacts on setup and teardown

--- EssentialDeveloper/EssentialDeveloperCacheIntegrationTests/EssentialDeveloperCacheIntegrationTests.swift
+++ EssentialDeveloper/EssentialDeveloperCacheIntegrationTests/EssentialDeveloperCacheIntegrationTests.swift
@@ -3,6 +3,17 @@ import EssentialDeveloper
 
 final class EssentialDeveloperCacheIntegrationTests: XCTestCase {
 
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
     func test_load_deliversNoItemsOnEmptyCache() {
             let sut = makeSUT()
 
@@ -31,6 +42,17 @@ final class EssentialDeveloperCacheIntegrationTests: XCTestCase {
             trackForMemoryLeaks(sut, file: file, line: line)
             return sut
         }
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
     
     private func testSpecificStoreURL() -> URL {
             return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
