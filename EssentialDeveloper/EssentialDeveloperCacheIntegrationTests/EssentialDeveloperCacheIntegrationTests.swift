baa0d6e3322eaed91012ec5462b46cf5df288961
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-01-29 12:43:05 +0000

    Fix integration tests

--- EssentialDeveloper/EssentialDeveloperCacheIntegrationTests/EssentialDeveloperCacheIntegrationTests.swift
+++ EssentialDeveloper/EssentialDeveloperCacheIntegrationTests/EssentialDeveloperCacheIntegrationTests.swift
@@ -3,88 +3,73 @@ import EssentialDeveloper
 
 final class EssentialDeveloperCacheIntegrationTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        
-        setupEmptyStoreState()
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+
+        try setupEmptyStoreState()
     }
     
-    override func tearDown() {
-        super.tearDown()
-        
-        undoStoreSideEffects()
+    override func tearDownWithError() throws {
+        try undoStoreSideEffects()
+
+        try super.tearDownWithError()
     }
     
-    func test_load_deliversNoItemsOnEmptyCache() {
-        let sut = makeSUT()
-        
-        expect(sut, toLoad: [])
+    func test_retrieve_deliversEmptyOnEmptyCache() throws {
+        let sut = try makeSUT()
+
+        expect(sut, toRetrieve: .empty)
     }
     
-    func test_load_deliversItemsSavedOnASeparateInstance() {
-        let sutToPerformSave = makeSUT()
-        let sutToPerformLoad = makeSUT()
-        let feed = uniqueImageFeed().models
-        
-        save(feed, with: sutToPerformSave)
-        
-        expect(sutToPerformLoad, toLoad: feed)
+    func test_retrieve_deliversFeedInsertedOnAnotherInstance() throws {
+        let storeToInsert = try makeSUT()
+        let storeToLoad = try makeSUT()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+
+        insert((feed, timestamp), to: storeToInsert)
+
+        expect(storeToLoad, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
     
-    func test_save_overridesItemsSavedOnASeparateInstance() {
-        let sutToPerformFirstSave = makeSUT()
-        let sutToPerformLastSave = makeSUT()
-        let sutToPerformLoad = makeSUT()
-        let firstFeed = uniqueImageFeed().models
-        let latestFeed = uniqueImageFeed().models
-        
-        save(firstFeed, with: sutToPerformFirstSave)
-        save(latestFeed, with: sutToPerformLastSave)
-        
-        expect(sutToPerformLoad, toLoad: latestFeed)
+    func test_insert_overridesFeedInsertedOnAnotherInstance() throws {
+        let storeToInsert = try makeSUT()
+        let storeToOverride = try makeSUT()
+        let storeToLoad = try makeSUT()
+
+        insert((uniqueImageFeed().local, Date()), to: storeToInsert)
+
+        let latestFeed = uniqueImageFeed().local
+        let latestTimestamp = Date()
+        insert((latestFeed, latestTimestamp), to: storeToOverride)
+
+        expect(storeToLoad, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+    }
+    
+    func test_delete_deletesFeedInsertedOnAnotherInstance() throws {
+        let storeToInsert = try makeSUT()
+        let storeToDelete = try makeSUT()
+        let storeToLoad = try makeSUT()
+
+        insert((uniqueImageFeed().local, Date()), to: storeToInsert)
+
+        deleteCache(from: storeToDelete)
+
+        expect(storeToLoad, toRetrieve: .empty)
     }
     
     // MARK: Helpers
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
-        let storeBundle = Bundle(for: CoreDataFeedStore.self)
-        let storeURL = testSpecificStoreURL()
-        let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
-        let sut = LocalFeedLoader(store: store, currentDate: Date.init)
-        trackForMemoryLeaks(store, file: file, line: line)
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) throws -> FeedStore {
+        let sut = try CoreDataFeedStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
     
-    private func save(_ feed: [FeedImage], with loader: LocalFeedLoader, file: StaticString = #file, line: UInt = #line) {
-        let saveExp = expectation(description: "Wait for save completion")
-        loader.save(feed) { saveError in
-            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
-            saveExp.fulfill()
-        }
-        wait(for: [saveExp], timeout: 1.0)
-    }
-    
-    private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(loadedFeed):
-                XCTAssertEqual(loadedFeed, expectedFeed, file: file, line: line)
-                
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-    }
-    
-    private func setupEmptyStoreState() {
+    private func setupEmptyStoreState() throws {
         deleteStoreArtifacts()
     }
     
-    private func undoStoreSideEffects() {
+    private func undoStoreSideEffects() throws {
         deleteStoreArtifacts()
     }
     
