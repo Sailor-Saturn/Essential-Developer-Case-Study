fab4b81b056fe795486fa1f7d51c2ae4076e451f
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-01 12:55:40 +0000

    Move Feed View Controller to production

--- EssentialDeveloper/EssentialDeveloperiOSTests/FeedViewControllerTests.swift
+++ EssentialDeveloper/EssentialDeveloperiOSTests/FeedViewControllerTests.swift
@@ -1,37 +1,7 @@
 import XCTest
 import UIKit
 import EssentialDeveloper
-
-final class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-    
-    private var onViewIsAppearing: ((UITableViewController) -> Void)?
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-    }
-    
-    public override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        refreshControl?.beginRefreshing()
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
+import EssentialDeveloperiOS
 
 final class FeedViewControllerTests: XCTestCase {
     
