2bce1bec83c9024ac16745fc6d23dfaad16359bb
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-03 08:22:45 +0000

    Extract UIRefresh control into a separate file

---
+++ EssentialDeveloper/EssentialDeveloperiOSTests/Helpers/UIRefreshControl+TestHelper.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach{ target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { (target as NSObject).perform(Selector($0)) }
+        }
+    }
+}
