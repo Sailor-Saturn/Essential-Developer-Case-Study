bb5d99fa65cb3aa0ac0a1e8e0248346b4ebde269
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-03 08:48:21 +0000

    Move FeedImageDataLoader into it's own file

--- EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Controllers/FeedViewController.swift
@@ -1,16 +1,6 @@
 import UIKit
 import EssentialDeveloper
 
-public protocol FeedImageDataLoaderTask {
-    func cancel()
-}
-
-public protocol FeedImageDataLoader {
-    typealias Result = Swift.Result<Data, Error>
-    
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
-}
-
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
   
     private var feedLoader: FeedLoader?
