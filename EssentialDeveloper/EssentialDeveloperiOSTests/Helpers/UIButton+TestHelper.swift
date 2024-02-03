00a0b4447e6ad82f94e54fea90db7c3cf1e644f7
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-03 08:23:53 +0000

    Extract UIButton Test Helper

---
+++ EssentialDeveloper/EssentialDeveloperiOSTests/Helpers/UIButton+TestHelper.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach{ target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach { (target as NSObject).perform(Selector($0)) }
+        }
+    }
+}
