fedea50b727b70481b8c5ba577b8f0d24e15e70f
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-03 08:31:35 +0000

    Extract feed image cell test helpers to a separate file

--- EssentialDeveloper/EssentialDeveloperiOSTests/FeedViewControllerTests.swift
+++ EssentialDeveloper/EssentialDeveloperiOSTests/FeedViewControllerTests.swift
@@ -337,37 +337,6 @@ final class FeedViewControllerTests: XCTestCase {
     }
 }
 
-private extension FeedImageCell {
-    var isShowingLocation: Bool {
-        return !locationContainer.isHidden
-    }
-    
-    var isShowingImageLoadingIndicator: Bool {
-        return feedImageContainer.isShimmering
-    }
-    
-    var isShowingRetryAction: Bool {
-        return !feedImageRetryButton.isHidden
-    }
-    
-    var locationText: String? {
-        return locationLabel.text
-    }
-    
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-    
-    var renderedImage: Data? {
-        return feedImageView.image?.pngData()
-    }
-    
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-
-}
-
 extension UITableViewController {
     
     func simulateAppearance() {
