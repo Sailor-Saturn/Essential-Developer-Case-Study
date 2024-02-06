0a288ff9cd8fcfcaf532675e476e8f9157a619a2
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-06 16:02:27 +0000

    Decouple feed presenter from the feed loader with an adapter in the compostion layer

--- EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Feed Presentation/FeedPresenter.swift
+++ EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Feed Presentation/FeedPresenter.swift
@@ -17,28 +17,19 @@ protocol FeedView {
 }
 
 final class FeedPresenter {
-    typealias Observer<T> = (T) -> Void
-    private let feedLoader: FeedLoader
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    var onLoadingStateChange: Observer<Bool>?
-    var onFeedLoad: Observer<[FeedImage]>?
-    
     var feedView: FeedView?
     var loadingView: FeedLoadingView?
     
-    func loadFeed() {
+    func didStartLoadingFeed() {
         loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: true))
-        
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.feedView?.display(viewModel: FeedViewModel(feed: feed))
-            }
-            
-            self?.loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
-        }
+    }
+    
+    func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView?.display(viewModel: FeedViewModel(feed: feed))
+        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
+    }
+    
+    func didFinishLoadingFeed(with error: Error) {
+        loadingView?.display(viewModel: FeedLoadingViewModel(isLoading: false))
     }
 }
