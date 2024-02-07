6977ebeb58d255c44ce691f2e0afaa37d8cb42f4
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-07 15:20:05 +0000

    Reuse table view cells from storyboard configuration

--- EssentialDeveloper/EssentialDeveloperiOSTests/FeedViewControllerTests.swift
+++ EssentialDeveloper/EssentialDeveloperiOSTests/FeedViewControllerTests.swift
@@ -240,7 +240,19 @@ final class FeedViewControllerTests: XCTestCase {
 
             sut.simulateFeedImageViewNotNearVisible(at: 1)
             XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
-        }
+    }
+    
+    func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+        let (sut, loader) = makeSUT()
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [makeImage()])
+        
+        let view = sut.simulateFeedImageViewNotVisible(at: 0)
+        
+        loader.completeImageLoading(with: anyImageData())
+        
+        XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore.")
+    }
     
     // MARK: Helpers
     
@@ -255,7 +267,10 @@ final class FeedViewControllerTests: XCTestCase {
     }
     
     private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "https://any-url.com")!) -> FeedImage {
-        return FeedImage(id: UUID(), description: description, location: location, url: url)
+        FeedImage(id: UUID(), description: description, location: location, url: url)
     }
     
+    private func anyImageData() -> Data {
+        UIImage.make(withColor: .red).pngData()!
+    }
 }
--- EssentialDeveloper/EssentialDeveloperiOSTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
+++ EssentialDeveloper/EssentialDeveloperiOSTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -47,7 +47,7 @@ extension FeedViewControllerTests {
             }
         }
         
-        func completeImageLoading(with imageData: Data = Data(), at index: Int) {
+        func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
             imageRequests[index].completion(.success(imageData))
         }
         
