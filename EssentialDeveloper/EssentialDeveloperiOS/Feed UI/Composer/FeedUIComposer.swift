e1767a84c38809d3527a3dbca5dbef1c91d32dca
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-02-07 10:24:05 +0000

    Replace closure event with delegate protocol

--- EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Composer/FeedUIComposer.swift
@@ -8,7 +8,7 @@ public final class FeedUIComposer {
         let presenter = FeedPresenter()
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
         
-        let refreshController = FeedRefreshViewController(loadFeed: presentationAdapter.loadFeed)
+        let refreshController = FeedRefreshViewController(delegate: presentationAdapter)
         let feedController = FeedViewController(refreshController: refreshController)
 
         presenter.loadingView = WeakReferenceVirtualProxy(refreshController)
@@ -56,7 +56,8 @@ private final class FeedViewAdapter: FeedView {
     }
 }
 
-private final class FeedLoaderPresentationAdapter {
+private final class FeedLoaderPresentationAdapter: FeedRefreshViewControllerDelegate {
+    
     private let feedLoader: FeedLoader
     private let presenter: FeedPresenter
     
@@ -65,7 +66,7 @@ private final class FeedLoaderPresentationAdapter {
         self.presenter = presenter
     }
     
-    func loadFeed() {
+    func didRequestFeedRefresh() {
         presenter.didStartLoadingFeed()
         
         feedLoader.load { [weak self] result in
--- EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialDeveloper/EssentialDeveloperiOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -1,16 +1,20 @@
 import UIKit
 
+protocol FeedRefreshViewControllerDelegate {
+    func didRequestFeedRefresh()
+}
+
 public final class FeedRefreshViewController: NSObject, FeedLoadingView {
     public lazy var view = loadView()
     
-    private let loadFeed: () -> Void
+    private let delegate: FeedRefreshViewControllerDelegate
     
-    init(loadFeed: @escaping () -> Void) {
-        self.loadFeed = loadFeed
+    init(delegate: FeedRefreshViewControllerDelegate) {
+        self.delegate = delegate
     }
     
     @objc func refresh() {
-        loadFeed()
+        delegate.didRequestFeedRefresh()
     }
     
     func display(viewModel: FeedLoadingViewModel) {
