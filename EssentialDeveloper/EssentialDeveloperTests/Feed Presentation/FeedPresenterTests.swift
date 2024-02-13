5605942a13c34854f692fd08338a62cbfb27d6bd
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-02-13 12:04:52 +0000

    Extract system under test to test helper function

--- EssentialDeveloper/EssentialDeveloperTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialDeveloper/EssentialDeveloperTests/Feed Presentation/FeedPresenterTests.swift
@@ -8,13 +8,22 @@ final class FeedPresenter {
 }
 
 final class FeedPresenterTests: XCTestCase {
-    func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
+    func test_init_doesNotSendMessagesToView() {        
+        let(_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages.")
     }
 
     // MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+        return (sut, view)
+    }
+    
     private class ViewSpy {
         let messages = [Any]()
     }
