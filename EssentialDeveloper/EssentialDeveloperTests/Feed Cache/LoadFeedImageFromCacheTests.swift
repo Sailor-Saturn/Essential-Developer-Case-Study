8d613ca5649b8a0fff6028983791bd9a0ff8664b
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-02-19 11:26:32 +0000

    Request from cahce retrieval returns retrieve from store

--- EssentialDeveloper/EssentialDeveloperTests/Feed Cache/LoadFeedImageFromCacheTests.swift
+++ EssentialDeveloper/EssentialDeveloperTests/Feed Cache/LoadFeedImageFromCacheTests.swift
@@ -1,14 +1,27 @@
 import XCTest
+import EssentialDeveloper
 
 protocol FeedImageStore {
-    
+    func retrieveImageData()
 }
 
-final class LocalImageFeedLoader {
+final class LocalImageFeedLoader: FeedImageDataLoader {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
     let store: FeedImageStore
 
     init(store: FeedImageStore) {
         self.store = store
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> EssentialDeveloper.FeedImageDataLoaderTask {
+        store.retrieveImageData()
+        
+        return Task()
     }
 }
 
@@ -17,6 +30,15 @@ final class LoadFeedImageFromCacheTests: XCTestCase {
         let (_, store) = makeSUT()
         
         XCTAssertTrue(store.receivedMessages.isEmpty)
+    }
+    
+    func test_load_requestsCacheRetrieval() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        
+        _ = sut.loadImageData(from: url) { _ in}
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
     private func makeSUT(
@@ -31,7 +53,15 @@ final class LoadFeedImageFromCacheTests: XCTestCase {
     }
     
     class FeedImageStoreSpy: FeedImageStore {
-        let receivedMessages = [Any]()
+        enum Message: Equatable {
+            case retrieve
+        }
+        
+        var receivedMessages = [Message]()
+        
+        func retrieveImageData() {
+            receivedMessages.append(.retrieve)
+        }
     }
     
 }
