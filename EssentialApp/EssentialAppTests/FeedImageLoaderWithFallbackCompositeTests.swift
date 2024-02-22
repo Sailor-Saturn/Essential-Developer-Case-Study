f0a08cff1f59eeb51ec500fc979d473d4dd1d728
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-02-22 17:02:43 +0000

    load should deliver error on both failures

--- EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositeTests.swift
@@ -107,6 +107,15 @@ final class FeedImageLoaderWithFallbackCompositeTests: XCTestCase {
         })
     }
     
+    func test_loadImageData_deliversErrorOnBothPrimaryAndFallbackLoaderFailure() {
+        let (sut, primaryLoader, fallbackLoader) = makeSUT()
+        
+        expect(sut, toCompleteWith: .failure(anyNSError()), when: {
+            primaryLoader.complete(with: anyNSError())
+            fallbackLoader.complete(with: anyNSError())
+        })
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImageDataLoader, primary: LoaderSpy, fallback: LoaderSpy) {
         let primaryLoader = LoaderSpy()
