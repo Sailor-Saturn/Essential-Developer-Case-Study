c5caaa5a1744f57a7081c353b892c763f89babe2
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-02 17:24:04 +0000

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness

--- EssentialApp/EssentialAppUITests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUITests/EssentialAppUIAcceptanceTests.swift
@@ -4,12 +4,12 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displayRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
@@ -17,7 +17,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
         
         onlineApp.launch()
         
@@ -26,7 +26,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         offlineApp.launch()
         
         let feedCells = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
