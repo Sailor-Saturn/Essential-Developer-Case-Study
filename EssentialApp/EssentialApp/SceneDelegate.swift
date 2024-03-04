eb1207691ce2bc68afb43ab57897e8471f55aaf9
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-04 17:18:31 +0000

    Configuring feed navigation as window root view controller

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -17,8 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appending(path: "feed-store.sqlite")
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        configureWindow()
+    }
+    
+    func configureWindow() {
         let remoteClient = makeRemoteClient()
         let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
    
@@ -30,9 +34,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let localFeedLoader = LocalFeedLoader(store: localFeedStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localFeedStore)
 
-        let feedViewController = FeedUIComposer.feedComposedWith(
+        let feedViewController = UINavigationController(rootViewController: FeedUIComposer.feedComposedWith(
             feedLoader: FeedLoaderWithFallbackComposite(primary: FeedLoaderCacheDecorator(decoratee: remoteFeedLoader, cache: localFeedLoader), fallback: localFeedLoader),
-            imageLoader: FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: FeedImageDataLoaderCacheDecorator(decoratee: remoteImageLoader, cache: localImageLoader)))
+            imageLoader: FeedImageDataLoaderWithFallbackComposite(primary: localImageLoader, fallback: FeedImageDataLoaderCacheDecorator(decoratee: remoteImageLoader, cache: localImageLoader))))
         
         window?.rootViewController = feedViewController
     }
