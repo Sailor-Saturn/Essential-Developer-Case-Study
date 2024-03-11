86040f894fedf168fea6b8e651609eec421da8d9
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-11 17:52:55 +0000

    Duplicate remote feed loader as remote image comments loader

---
+++ EssentialDeveloper/EssentialDeveloper/Feed API/ImageCommentsMapper.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+internal final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+
+    private static var OK_200: Int { return 200}
+    
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data)
+        else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+
+        return root.items
+    }
+}
