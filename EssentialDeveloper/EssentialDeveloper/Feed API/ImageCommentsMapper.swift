ee36c264b8be127f26e83884f01c6fa68809f1dc
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-14 17:09:51 +0000

    implement image comment mapping

--- EssentialDeveloper/EssentialDeveloper/Feed API/ImageCommentsMapper.swift
+++ EssentialDeveloper/EssentialDeveloper/Feed API/ImageCommentsMapper.swift
@@ -2,18 +2,38 @@ import Foundation
 
 internal final class ImageCommentsMapper {
     private struct Root: Decodable {
-        let items: [RemoteFeedItem]
+        private let items: [Item]
+        
+        private struct Item: Decodable{
+            let id: UUID
+            let message: String
+            let created_at: Date
+            let author: Author
+        }
+        
+        private struct Author: Decodable {
+            let username: String
+        }
+        
+        var comments: [ImageComment] {
+            items.map { item in
+                ImageComment(id: item.id, message: item.message, createdAt: item.created_at, username: item.author.username)
+            }
+        }
     }
 
     
-    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [ImageComment] {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
         guard isOk(response),
-              let root = try? JSONDecoder().decode(Root.self, from: data)
+              let root = try? decoder.decode(Root.self, from: data)
         else {
             throw RemoteImageCommentsLoader.Error.invalidData
         }
 
-        return root.items
+        return root.comments
     }
     
     private static func isOk(_ response: HTTPURLResponse) -> Bool {
