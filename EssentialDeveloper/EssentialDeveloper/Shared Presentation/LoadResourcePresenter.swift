8852e709a6989b7c136f869bc891d8ea897292e7
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-22 17:34:45 +0000

    Move "GENERIC_CONNECTION_ERROR" locatization string to shared module

--- EssentialDeveloper/EssentialDeveloper/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialDeveloper/EssentialDeveloper/Shared Presentation/LoadResourcePresenter.swift
@@ -31,7 +31,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     }
     
     public func didFinishLoading(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
+        errorView.display(.error(message: Self.loadError))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
 }
@@ -40,10 +40,10 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
 
 // MARK: - Localized Strings
 extension LoadResourcePresenter {
-    private var feedLoadError: String {
+    public static var loadError: String {
         return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-                                 tableName: "Feed",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "Error message displayed when we can't load the image feed from the server")
+                                 tableName: "Shared",
+                                 bundle: Bundle(for: Self.self),
+                                 comment: "Error message displayed when we can't load from the server")
     }
 }
--- EssentialDeveloper/EssentialDeveloperTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialDeveloper/EssentialDeveloperTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -50,7 +50,7 @@ final class LoadResourcePresenterTests: XCTestCase {
     }
     
     private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-        let table = "Feed"
+        let table = "Shared"
         let bundle = Bundle(for: SUT.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         
