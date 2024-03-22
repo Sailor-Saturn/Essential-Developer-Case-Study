e1239aa46f8cdc895cff3b09c6412d8748387a14
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-22 19:16:39 +0000

    Removed unused feed presenter logic

--- EssentialDeveloper/EssentialDeveloper/Feed Presentation/Feed /FeedPresenter.swift
+++ EssentialDeveloper/EssentialDeveloper/Feed Presentation/Feed /FeedPresenter.swift
@@ -1,49 +1,6 @@
 import Foundation
 
-public protocol FeedView {
-    func display(_ viewModel: FeedViewModel)
-}
-
 public final class FeedPresenter {
-    private let errorView: ResourceErrorView
-    private let loadingView: ResourceLoadingView
-    private let feedView: FeedView
-    
-    public init(errorView: ResourceErrorView, loadingView: ResourceLoadingView, feedView: FeedView){
-        self.errorView = errorView
-        self.loadingView = loadingView
-        self.feedView = feedView
-    }
-    
-    public func didStartLoadingFeed() {
-        errorView.display(.noError)
-        loadingView.display(ResourceLoadingViewModel(isLoading: true))
-    }
-    
-    public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(Self.map(feed))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-    
-    public func didFinishLoadingFeed(with error: Error) {
-        errorView.display(.error(message: feedLoadError))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
-    }
-    
-    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
-        FeedViewModel(feed: feed)
-    }
-}
-
-// MARK: - Localized Strings
-extension FeedPresenter {
-    private var feedLoadError: String {
-        return NSLocalizedString("GENERIC_CONNECTION_ERROR",
-                                 tableName: "Shared",
-                                 bundle: Bundle(for: FeedPresenter.self),
-                                 comment: "Error message displayed when we can't load the image feed from the server")
-    }
-    
     public static var title: String {
         return NSLocalizedString(
             "FEED_VIEW_TITLE",
@@ -53,5 +10,9 @@ extension FeedPresenter {
             ),
             comment: "Title for the feed view."
         )
+    }
+    
+    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
+        FeedViewModel(feed: feed)
     }
 }
