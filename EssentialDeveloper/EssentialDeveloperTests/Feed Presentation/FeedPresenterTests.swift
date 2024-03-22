90de4d70fd11200996976da8e49c95cddc9cbf2b
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-22 19:17:22 +0000

    add inline parameter

--- EssentialDeveloper/EssentialDeveloperTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialDeveloper/EssentialDeveloperTests/Feed Presentation/FeedPresenterTests.swift
@@ -16,7 +16,8 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertEqual(viewModel.feed, feed)
     }
     
-    private func localized(_ key: String, table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         
