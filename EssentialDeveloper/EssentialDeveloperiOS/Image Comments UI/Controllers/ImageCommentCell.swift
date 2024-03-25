c09277c8847ec5e2e5ab9643fdbb36c79c61f430
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-25 14:37:44 +0000

    Implement Image comments UI

---
+++ EssentialDeveloper/EssentialDeveloperiOS/Image Comments UI/Controllers/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialDeveloperiOS
+//
+//  Created by Vera Dias on 25/03/2024.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    @IBOutlet private(set) public var messageLabel: UILabel!
+}
---
+++ EssentialDeveloper/EssentialDeveloperiOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,28 @@
+import UIKit
+import EssentialDeveloper
+
+public class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+    
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+    
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        
+        cell.messageLabel.text = model.message
+        cell.dateLabel.text = model.date
+        cell.usernameLabel.text = model.username
+        
+        return cell
+    }
+    
+    public func preload() {
+        
+    }
+    
+    public func cancelLoad() {
+        
+    }
+}
