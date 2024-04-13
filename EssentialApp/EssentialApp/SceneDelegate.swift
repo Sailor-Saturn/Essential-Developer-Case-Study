c2597386d23498c73502c1e67f10d1a6cc9152b1
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-04-13 19:16:09 +0100

    Add network logs

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -128,6 +128,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         return localImageLoader
             .loadImageDataPublisher(from: url)
+            .logCacheMisses(url: url, logger: logger)
             .fallback(to: { [httpClient, logger] in
                 httpClient
                     .getPublisher(url: url)
@@ -140,6 +141,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 }
 
 extension Publisher {
+    func logCacheMisses(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
+        handleEvents( receiveCompletion: { result in
+            if case .failure = result {
+                logger.trace("Cache miss for url: \(url)")
+            }
+        }).eraseToAnyPublisher()
+    }
+    
     func logError(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
         handleEvents( receiveCompletion: { result in
             if case let .failure(error) = result {
